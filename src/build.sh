a3381bd272497b58b04c1adaa25b0a2e9e4c7031
Zijian Xu <xuzijian@g.ecc.u-tokyo.ac.jp>
2020-04-02 03:02:15 +0900

    path agnostic

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,6 @@
 ##### EDIT HERE #####
 BASE_FILE=sample.cpp
 #####################
+src_dir=$(dirname $0)
 
-g++ -O2 -std=c++17 -o main $BASE_FILE
+g++ -O2 -std=c++17 -o $src_dir/main $src_dir/$BASE_FILE
