e94b8905931fbd29ae3478ca8086958bb773f233
Zijian Xu <xuzijian@g.ecc.u-tokyo.ac.jp>
2020-04-20 00:02:28 +0900

    rename

--- src/lib/graph.cpp
+++ src/lib/graph.cpp
@@ -4,59 +4,67 @@ using namespace std;
 
 struct Graph {
     int n, m;
-    vector<vector<int>> adj;
-    // 構築時に必要になるため、分解後のグラフでも入力グラフでの頂点番号を保持しておく
-    vector<int> original_index;
-    Graph() {}
-    Graph(int n) : n(n), m(0), adj(n), original_index(n) { iota(original_index.begin(), original_index.end(), 0); }
+    // for rooted tree
+    int root;
+    unordered_set<int> nodes;
+    unordered_map<int, unordered_set<int>> adj;
+    Graph() : n(0), m(0), root(-1) {}
+    Graph(int v) : n(0), m(0), root(v) { add_node(v); }
+    void add_node(int v) {
+        if (!adj.count(v)) {
+            n++;
+            adj[v];
+            nodes.insert(v);
+        }
+    }
     void add_edge(int a, int b) {
-        assert(0 <= a && a < n);
-        assert(0 <= b && b < n);
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-        m++;
+        add_node(a);
+        add_node(b);
+        if (!adj[a].count(b)) {
+            m++;
+            adj[a].insert(b);
+            adj[b].insert(a);
+        }
+    }
+    void remove_node(int v) {
+        n--;
+        assert(adj.count(v));
+        for (int a : adj[v]) {
+            adj[a].erase(v);
+            m--;
+        }
+        adj.erase(v);
+        nodes.erase(v);
+    }
+    void remove_edge(int a, int b) {
+        assert(adj[a].count(b) && adj[b].count(a));
+        adj[a].erase(b);
+        adj[b].erase(a);
+        m--;
     }
 };
 
 void print_graph(const Graph& g) {
     cout << "n=" << g.n << ", m=" << g.m << ", nodes={";
-    for (int i = 0; i < g.n; i++) {
-        cout << g.original_index[i] << ",";
+    for (int v : g.nodes) {
+        cout << v << ",";
     }
     cout << "}, edges={";
-    for (int a = 0; a < g.n; a++) {
-        for (int b : g.adj[a]) {
+    for (int a : g.nodes) {
+        for (int b : g.adj.at(a)) {
             if (a < b) {
-                cout << "(" << g.original_index[a] << "," << g.original_index[b] << "),";
+                cout << "(" << a << "," << b << "),";
             }
         }
     }
     cout << "}" << endl;
 }
 
-Graph read_input() {
-    string s;
-    int n, m;
-    cin >> s >> s >> n >> m;
-    Graph ret(n);
-    for (int i = 0; i < m; i++) {
-        int a, b;
-        cin >> a >> b;
-        a--, b--;
-        ret.add_edge(a, b);
-    }
-    return ret;
-}
-
-// O(m)
-// 木の深さを求める
-int depth(const Graph& tree, int root = 0) {
-    assert(root < tree.n);
-    assert(tree.m == tree.n - 1);
+int depth(const Graph& tree, int root) {
     int ret = 0;
     auto dfs = [&](auto& dfs, int v, int p, int d) -> void {
         ret = max(ret, d);
-        for (auto& s : tree.adj[v]) {
+        for (auto& s : tree.adj.at(v)) {
             if (s != p) {
                 dfs(dfs, s, v, d + 1);
             }
@@ -66,106 +74,72 @@ int depth(const Graph& tree, int root = 0) {
     return ret;
 }
 
-// O(m)
-// グラフ g から頂点 u を削除して得られるグラフを連結成分ごとに返す
-// 頂点番号は各連結成分ごとに 0 から振り直されている
-vector<Graph> remove(const Graph& g, int u) {
-    vector<int> color(g.n, -1);
-    auto dfs = [&](auto& dfs, int v, int c) -> void {
-        color[v] = c;
-        for (int s : g.adj[v]) {
-            if (s == u) continue;
-            if (color[s] == -1) dfs(dfs, s, c);
-        }
-    };
-    int c = 0;
-    for (int i = 0; i < g.n; i++) {
-        if (i == u) continue;
-        if (color[i] == -1) {
-            dfs(dfs, i, c);
-            c++;
-        }
-    }
-    vector<int> node_cnt(c);
-    for (int i = 0; i < g.n; i++) {
-        if (i == u) continue;
-        node_cnt[color[i]]++;
-    }
-    vector<Graph> ret(c);
-    for (int i = 0; i < c; i++) ret[i] = Graph(node_cnt[i]);
-    node_cnt.assign(c, 0);
-    vector<int> new_node_id(g.n, -1);
-    for (int a = 0; a < g.n; a++) {
-        for (int b : g.adj[a]) {
-            if (a == u || b == u) continue;
-            if (a < b) {
-                assert(color[a] == color[b]);
-                if (new_node_id[a] == -1) {
-                    new_node_id[a] = node_cnt[color[a]]++;
-                }
-                if (new_node_id[b] == -1) {
-                    new_node_id[b] = node_cnt[color[b]]++;
-                }
-                ret[color[a]].add_edge(new_node_id[a], new_node_id[b]);
-            }
-        }
-    }
-    for (int i = 0; i < g.n; i++) {
-        // 孤立点の id は 0
-        if (new_node_id[i] == -1) new_node_id[i] = 0;
-    }
-    for (int i = 0; i < g.n; i++) {
-        if (i == u) continue;
-        ret[color[i]].original_index[new_node_id[i]] = g.original_index[i];
-    }
-    return ret;
-}
-
-// O(m)
-// root (original index) と、各 subtree の根を結んだ新たな木をつくる
-Graph merge_subtrees(int root, const vector<Graph>& subtrees) {
-    int n = 1;
-    for (auto& g : subtrees) {
-        n += g.n;
-    }
-    Graph ret(n);
-    ret.original_index[0] = root;
-    n = 1;
-    for (auto& g : subtrees) {
-        ret.add_edge(0, n);
-        for (int a = 0; a < g.n; a++) {
-            ret.original_index[n + a] = g.original_index[a];
-            for (int b : g.adj[a]) {
-                if (a < b) {
-                    ret.add_edge(n + a, n + b);
-                }
-            }
-        }
-        n += g.n;
-    }
-    assert(ret.m == ret.n - 1);
-    return ret;
-}
-
-// treedepth decomposition を出力する
 void print_decomp(const Graph& decomp) {
     assert(decomp.m == decomp.n - 1);
-    cout << depth(decomp) << endl;
+    cout << depth(decomp, decomp.root) << endl;
     vector<int> par(decomp.n);
     auto dfs = [&](auto& dfs, int v, int p) -> void {
-        if (p == -1) {
-            par[decomp.original_index[v]] = -1;
-        } else {
-            par[decomp.original_index[v]] = decomp.original_index[p];
-        }
-        for (auto& s : decomp.adj[v]) {
-            if (s != p) {
-                dfs(dfs, s, v);
-            }
+        par[v] = p;
+        for (auto& s : decomp.adj.at(v)) {
+            if (s != p) dfs(dfs, s, v);
         }
     };
-    dfs(dfs, 0, -1);
-    for (int i = 0; i < decomp.n; i++) {
-        cout << par[i] + 1 << '\n';
+    dfs(dfs, decomp.root, -1);
+    for (int v = 0; v < decomp.n; v++) {
+        cout << par[v] + 1 << '\n';
     }
+}
+
+Graph read_input() {
+    string s;
+    int n, m;
+    cin >> s >> s >> n >> m;
+    Graph ret;
+    for (int v = 0; v < n; v++) ret.add_node(v);
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        a--, b--;
+        ret.add_edge(a, b);
+    }
+    return ret;
+}
+
+// merge g2 into g1
+void merge(Graph& g1, Graph& g2, int u, int v) {
+    assert(g1.nodes.count(u) && g2.nodes.count(v));
+    int root = g1.root;
+    if (g1.n < g2.n) swap(g1, g2);
+    for (int a : g2.nodes) {
+        for (int b : g2.adj.at(a)) {
+            if (a < b) g1.add_edge(a, b);
+        }
+    }
+    g1.add_edge(u, v);
+    g1.root = root;
+}
+
+vector<Graph> components(const Graph& g) {
+    int color_cnt = 0;
+    unordered_map<int, int> color(g.n);
+    auto dfs = [&](auto& dfs, int v, int c) -> void {
+        color[v] = c;
+        for (auto& s : g.adj.at(v)) {
+            if (!color.count(s)) dfs(dfs, s, c);
+        }
+    };
+    for (int v : g.nodes) {
+        if (!color.count(v)) dfs(dfs, v, color_cnt++);
+    }
+    vector<Graph> ret(color_cnt);
+    for (int a : g.nodes) {
+        ret[color[a]].add_node(a);
+        for (int b : g.adj.at(a)) {
+            if (a < b) {
+                assert(color[a] == color[b]);
+                ret[color[a]].add_edge(a, b);
+            }
+        }
+    }
+    return ret;
 }
