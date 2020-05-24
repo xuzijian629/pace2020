bc294895cfc4771677b326fdeb1beafc8047a429
Zijian Xu <xuzijian@g.ecc.u-tokyo.ac.jp>
2020-05-24 15:21:14 +0900

    revert changes (not very working)

--- src/lib/partition/balanced.cpp
+++ src/lib/partition/balanced.cpp
@@ -9,35 +9,30 @@ int treedepth_exact(const Graph& g) {
     return depth(decomp, decomp.root);
 }
 
+constexpr int min_n = 15;
 constexpr int max_n = 30;
 constexpr int max_precompute_num = 500;
 
 vector<BITSET> BLOCKS;
 vector<int> BLOCK_TD;
 
-void decompose(const Graph& g, double alpha, int max_size) {
-    int min_size = (max_size + 1) / 2;
-    if (g.n() <= max_size) {
-        if (g.n() >= min_size) BLOCKS.push_back(g.nodes);
+void decompose(const Graph& g, double alpha) {
+    if (g.n() <= max_n) {
+        if (g.n() >= min_n) BLOCKS.push_back(g.nodes);
         return;
     }
     auto sep = GA(g, alpha);
     for (auto& C : components(remove(g, sep))) {
-        decompose(induced(g, C), alpha, max_size);
+        decompose(induced(g, C), alpha);
     }
 }
 
 void init_blocks(const Graph& g) {
     double alpha = 0.5;
-    int max_size = max_n;
-    for (int _ = 0; _ < 5; _++) {
-        for (int i = 0; i < max_precompute_num; i++) {
-            decompose(g, alpha, max_size);
-            alpha += 0.01;
-            if (alpha > 0.8) alpha = 0.5;
-        }
-        if (BLOCKS.size() >= max_precompute_num) break;
-        max_size *= 0.9;
+    for (int i = 0; i < max_precompute_num; i++) {
+        decompose(g, alpha);
+        alpha += 0.01;
+        if (alpha > 0.8) alpha = 0.5;
     }
     sort(BLOCKS.begin(), BLOCKS.end());
     BLOCKS.erase(unique(BLOCKS.begin(), BLOCKS.end()), BLOCKS.end());
