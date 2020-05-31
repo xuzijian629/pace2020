19682a10f8e61de51f44cf74c96f2cfd18b8bd10
Zijian Xu <xuzijian@g.ecc.u-tokyo.ac.jp>
2020-06-01 01:24:16 +0900

    use blocks in prep

--- src/lib/partition/balanced.cpp
+++ src/lib/partition/balanced.cpp
@@ -4,10 +4,10 @@
 #include "fakeflow.cpp"
 #include "flow.cpp"
 
-extern Graph treedepth_decomp(Graph g, bool use_block);
+extern Graph treedepth_decomp(Graph g, int use_block_max_size);
 
-int treedepth_exact(const Graph& g) {
-    Graph decomp = treedepth_decomp(g, false);
+int treedepth_exact(const Graph& g, int use_block_max_size) {
+    Graph decomp = treedepth_decomp(g, use_block_max_size);
     return depth(decomp, decomp.root);
 }
 
@@ -84,13 +84,13 @@ void init_blocks(const Graph& g, int tl_millis) {
     prepare_d_flow(g);
     gen_blocks(g, nax);
 
-    for (int i = 0; i <= nax; i++) {
+    for (int i = 1; i <= nax; i++) {
         int sz = BLOCKS[i].size();
         if (!sz) continue;
         auto start = chrono::steady_clock::now();
         vector<pair<BITSET, int>> tmp;
         for (int j = 0; j < sz; j++) {
-            int d = treedepth_exact(induced(g, BLOCKS[i][j]));
+            int d = treedepth_exact(induced(g, BLOCKS[i][j]), i - 1);
             tmp.emplace_back(BLOCKS[i][j], d);
         }
         sort(tmp.begin(), tmp.end(), [](auto& a, auto& b) { return a.second > b.second; });
